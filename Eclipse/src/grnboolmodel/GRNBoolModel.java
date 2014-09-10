364ee641913ae59fdf13f8a5bd06fb954be940cd
wlongabaugh <wlongabaugh@systemsbiology.org>
2014-09-10 15:53:07 -0700

    Initial Check-in Test with ISB Annotation

--- Eclipse/src/grnboolmodel/GRNBoolModel.java
+++ Eclipse/src/grnboolmodel/GRNBoolModel.java
@@ -16,6 +16,12 @@
 **    You should have received a copy of the GNU Lesser General Public
 **    License along with this library; if not, write to the Free Software
 **    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+**
+**    GeNeTool Port to Eclipse Framework: William Longabaugh
+**    Copyright (C) 2014 Institute for Systems Biology 
+**                       Seattle, Washington, USA. 
+**
+**
 */
 
 package grnboolmodel;
